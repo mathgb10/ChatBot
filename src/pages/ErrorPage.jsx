13cea4119090b7e8b1fe213d47e8cbcbee4fdec1
Matheus G Benevides <matheusgoncalvesbenevides@gmail.com>
2026-03-11 14:00:00 -0300

    del: removi a opcao de alterara tema na errorpage

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,22 +1,15 @@
 import BackgroundSG from "../components/BackgroundSG";
 import { FaGear } from "react-icons/fa6";
-import { useState, useEffect } from "react";
 
 export default function ErrorPage() {
-
-    const [tema, setTema] = useState(() => { return localStorage.getItem('tema_chatbot') == 'true' || false })
-    useEffect(() => {
-        localStorage.setItem('tema_chatbot', tema)
-    }, [tema])
-
     return (
         <>
-            <BackgroundSG tema={tema} />
+            <BackgroundSG />
             <main className="w-full min-h-screen flex justify-center items-center">
                 <div className="w-fit flex flex-col gap-5 items-center justify-center text-2xl text-white absolute">
                     <FaGear className="text-8xl animate-spin" />
                     <div className="text-center">
-                        <h2>404 Error</h2>
+                        <h2 className="text-rose-600">404 Error</h2>
                         <p className="text-lg">Página não encontrada</p>
                     </div>
                 </div>
