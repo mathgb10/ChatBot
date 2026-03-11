22c63f133f6292d56f1d0d6aafb23d12639b9516
Matheus G Benevides <matheusgoncalvesbenevides@gmail.com>
2026-03-11 14:00:40 -0300

    del: removi a opcao de alterara tema na loginpage

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,22 +1,16 @@
-import { useState, useEffect } from 'react'
 import BackgroundSG from '../components/BackgroundSG'
 import SizeBox from '../components/SizeBox'
 
 export default function LoginPage() {
 
-    const [tema, setTema] = useState(()=>{return localStorage.getItem('tema_chatbot') == 'true' || false})
-    useEffect(()=>{
-        localStorage.setItem('tema_chatbot',tema)
-    },[tema])
-
     return (
         <>
-            <BackgroundSG tema={tema} />
+            <BackgroundSG />
             <main className='w-full min-h-screen flex justify-center items-center'>
                 <section className="w-full h-full flex justify-center items-center">
                     <div className='w-9/12 p-5 flex items-center justify-around rounded-lg border-1 border-white/25 bg-white/20'>
-                        <SizeBox conteudo='LadoUm'/>
-                        <SizeBox conteudo='LadoDois'/>
+                        <SizeBox conteudo='LadoUm' />
+                        <SizeBox conteudo='LadoDois' />
                     </div>
                 </section>
             </main>
